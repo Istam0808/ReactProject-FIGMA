e19d95dc21e4cc0b51bda867a132a2b6b4a5b0a3
Istam <mamadaliyevistam@gmail.com>
2025-04-16 17:51:56 +0500

    Первый коммит

---
+++ src/app/layout.jsx
@@ -0,0 +1,12 @@
+import Nav from "./components/Nav";
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <Nav/>
+        {children}
+      </body>
+    </html>
+  );
+}
