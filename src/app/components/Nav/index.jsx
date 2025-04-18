e95b9d9632e8a9d16f24086f340a6395b8cc4633
Istam <mamadaliyevistam@gmail.com>
2025-04-18 16:50:27 +0500

    update

--- src/app/components/Nav/index.jsx
+++ src/app/components/Nav/index.jsx
@@ -1,7 +1,29 @@
+import Link from "next/link";
+import "./style.scss";
+import Image from "next/image";
+import Logo from "../../../assets/icons/logo.png"
+
 function Nav() {
-    return ( 
-        <h1>nav</h1>
-     );
+    return (
+        <nav>
+            <div className="left">
+                <Link href="/" />
+                <Image src={Logo} width={100} height={100} alt="logo" />
+            </div>
+            <div className="right">
+                <div className="right-left">
+                    <Link href="/">Home</Link>
+                    <Link href="/about">About</Link>
+                    <Link href="/contact">Contact</Link>
+                    <Link href="/blog">Blog</Link>
+                </div>
+                <div className="right-right">
+                    <button>ВОЙТИ</button>
+                    <p>РУС</p>
+                </div>
+            </div>
+        </nav>
+    );
 }
 
 export default Nav;
